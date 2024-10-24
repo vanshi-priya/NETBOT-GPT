f00c8159f024065f1d55ab8a827816f842431236
Vanshika Srivastava <vanshi27aug@gmail.com>
2024-10-24 16:37:42 +0530

    last commit

---
+++ src/utils/firebase.js
@@ -0,0 +1,51 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// import { getAuth } from "firebase/auth";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBcDCYpIEZNsf23x0Bb1RaQAsivUw-i7eA",
+//   authDomain: "netflixgpt-2801.firebaseapp.com",
+//   projectId: "netflixgpt-2801",
+//   storageBucket: "netflixgpt-2801.appspot.com",
+//   messagingSenderId: "995857549729",
+//   appId: "1:995857549729:web:ec1211962f9b3e88e323e0",
+//   measurementId: "G-JYR8FL6E08"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+// export const auth = getAuth();
+
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAI3p9WAKjuqX4lFdiZD1jOC2p0iGBYYiU",
+  authDomain: "netflixgpt-3575b.firebaseapp.com",
+  projectId: "netflixgpt-3575b",
+  storageBucket: "netflixgpt-3575b.appspot.com",
+  messagingSenderId: "595932970146",
+  appId: "1:595932970146:web:5600da3d5b7b3f863b2bfb",
+  measurementId: "G-K9KC462NPG"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth();
